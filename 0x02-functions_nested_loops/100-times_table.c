940618ee92c93341e10f25bea8dec9294c5ab10c
Ini <iniaugustineokonko@gmail.com>
2023-09-13 09:50:44 -0700

    Updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,7 @@
 #include "main.h"
-
-
-/**
-* print_times_table - prints the n times table, starting with 0
-* @n: number of the times table
-*/
 void print_times_table(int n)
 {
 int i, j, k;
-
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
@@ -44,5 +36,6 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
+}
 }
 }
